a23a2c16256f444459beb9400fbfaee289df1c12
carsonhuang <981347807@qq.com>
2021-03-17 19:32:00 +0800

    完成JZ31、32

--- nowcoder/JZ31.js
+++ nowcoder/JZ31.js
@@ -1,26 +1,51 @@
+// function NumberOf1Between1AndN_Solution(n) {
+//   if (n === 0) {
+//     return 0
+//   }
+//   let res = 0
+//   for (let i = 1; i <= n; i++) {
+//     res += count(i)
+//   }
+//   return res
+// }
+// function count(n) {
+//   let sum = 0
+//   while (n) {
+//     let yu = n % 10
+//     if (yu === 1) {
+//       sum++
+//     }
+//     n = Math.floor(n / 10)
+//   }
+//   return sum
+// }
+
 function NumberOf1Between1AndN_Solution(n) {
   if (n === 0) {
     return 0
   }
   let res = 0
-  for (let i = 1; i <= n; i++) {
-    res += count(i)
+  let current
+  let before
+  let after
+  let i = 1
+  while (Math.floor(n / i) !== 0) {
+    current = Math.floor(n / i) % 10
+    before = Math.floor(n / (i * 10))
+    after = n - Math.floor(n / i) * i
+    if (current === 0) {
+      res += before * i
+    } else if (current === 1) {
+      res += before * i + after + 1
+    } else {
+      res += (before + 1) * i
+    }
+    i = i * 10
   }
   return res
 }
-function count(n) {
-  let sum = 0
-  while (n) {
-    let yu = n % 10
-    if (yu === 1) {
-      sum++
-    }
-    n = Math.floor(n / 10)
-  }
-  return sum
-}
 
-console.log(NumberOf1Between1AndN_Solution(13))
+console.log(NumberOf1Between1AndN_Solution(12012))
 
 module.exports = {
   NumberOf1Between1AndN_Solution: NumberOf1Between1AndN_Solution,
--- nowcoder/JZ32.js
+++ nowcoder/JZ32.js
@@ -1,39 +1,46 @@
+// function PrintMinNumber(numbers) {
+//   let res = []
+//   const helper = (track) => {
+//     if (track.length === numbers.length) {
+//       res.push([...track].join(''))
+//     }
+//     for (let i = 0; i < numbers.length; i++) {
+//       if (track.indexOf(numbers[i]) !== -1) {
+//         continue
+//       }
+//       track.push(numbers[i])
+//       helper(track)
+//       track.pop()
+//     }
+//   }
+//   helper([])
+//   let result = res[0]
+//   for (let i = 1; i < res.length; i++) {
+//     if (result > res[i]) {
+//       result = res[i]
+//     }
+//   }
+//   console.log(res)
+//   return result
+// }
+
 function PrintMinNumber(numbers) {
-  let res = []
-  const helper = (track) => {
-    if (track.length === numbers.length) {
-      res.push([...track].join(''))
+  numbers.sort((a, b) => {
+    if ('' + a + '' + b < '' + b + '' + a) {
+      return -1
+    } else {
+      return 1
     }
-    for (let i = 0; i < numbers.length; i++) {
-      if (track.indexOf(numbers[i]) !== -1) {
-        continue
-      }
-      track.push(numbers[i])
-      helper(track)
-      track.pop()
-    }
+  })
+  let res = ''
+  for (let i = 0; i < numbers.length; i++) {
+    res += numbers[i]
   }
-  helper([])
-  let result = res[0]
-  for (let i = 1; i < res.length; i++) {
-    if (!compare(result, res[i])) {
-      result = res[i]
-    }
-  }
-  console.log(res)
-  return result
+  console.log(numbers)
+  return res
 }
 
-function compare(num1, num2) {
-  for (let i = 0; i < num1.length; i++) {
-    if (num1[i] < num2[i]) {
-      return true
-    }
-  }
-  return false
-}
-
-console.log(PrintMinNumber([3, 5, 1, 4, 2]))
+console.log(PrintMinNumber([3, 32, 321]))
 
 module.exports = {
   PrintMinNumber: PrintMinNumber,
